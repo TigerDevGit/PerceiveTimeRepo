3f76912ad4b7e7623db3ee5fde6d9a32aa61b949
i.pylypko <i.pylypko@bvblogic.com>
2016-01-22 18:30:24 +0200

    Add business landing page v2
    This is a complete rewrite of the original business landing page

--- app/static-pages/business/lib/js/main.js
+++ app/static-pages/business/lib/js/main.js
@@ -1,193 +1,92 @@
 $(document).ready(function(){
-  $.clickyConfigureAllGoals(100857897);
 
-  // open Form
-  $('body').on('click', '.show_contact_form', function(){
-    $('.hidden_contact_form').slideToggle(600);
+  integrationCarousel();
+  smoothScroll();
 
-    var dest = $('#contact').offset().top;
-    $('html, body').animate({'scrollTop' : dest - 70}, 600);
+});
+
+
+function integrationCarousel(){
+
+  var carousel = $('.integrations__carousel--items'),
+      carouselItem = $('.integrations__carousel--items').children(),
+      carouselAnimation = 5000,
+      carouselItemWidth = carouselItem.innerWidth();
+
+  function carouselWidth(){
+    carousel.width( carouselItemWidth * $('.integrations__carousel--items > div:visible').length );
+  }
+  carouselWidth();
+
+  function loop(){
+    $(carousel).stop().animate({'margin-left': -( carouselItemWidth )}, carouselAnimation, 'linear', function(){
+      $(this).css({'margin-left': 0}).find('div:last').after($('div:first', this));
+      loop();
+    });
+  }
+  loop();
+
+  function filterCarousel(match){
+    if( match == '' ){
+      $('.no__match').hide();
+      carouselItem.show();
+      $('.integrations__carousel--frame').removeClass('carousel__filtered');
+      carouselWidth();
+    } else {
+      carouselItem.hide();
+      carousel.find('[data-logo-name*=\"'+match+'\"]').show();
+      $('.integrations__carousel--frame').addClass('carousel__filtered');
+      carouselWidth();
+
+      if( !$('[data-logo-name*=\"'+match+'\"]').length ){
+        $('.no__match').show();
+      }else{
+        $('.no__match').hide();
+      }
+    }
+  }
+
+  var keyupTimeout;
+  $('.slider__section--input').keyup(function(){
+    var value = $(this).val(),
+        value = value.toLowerCase();
+
+    window.clearTimeout(keyupTimeout);
+
+    keyupTimeout = setTimeout(function(){
+      filterCarousel(value);
+    }, 300);
+
+  });
+
+}
+
+function smoothScroll(){
+  $('body').on('click', '.smooth__scroll', function(){
+    var elementClick = $(this).attr('href'),
+        destination = $(elementClick).offset().top;
+
+    if ( elementClick == '#section_more' ){
+      $('.pricing__section').addClass('active_index');
+    }
+
+    $('html, body').animate({ scrollTop: destination-100}, 800, function(){
+      if( elementClick == '#section_pricing' ){
+        $('.pricing__section').addClass('active_pricing');
+        setTimeout(function(){
+          $('.pricing__section').removeClass('active_pricing');
+        }, 1000);
+      } else if ( elementClick == '#section_more' ){
+        $('.pricing__section').addClass('active_more');
+        setTimeout(function(){
+          $('.pricing__section').removeClass('active_more');
+        }, 1000);
+        setTimeout(function(){
+          $('.pricing__section').removeClass('active_index')
+        }, 1150);
+      }
+    });
+
     return false;
   })
-
-})
-
-// begin jquery.sticky script
-$(window).load(function(){
-  $("#header_wrapper").sticky({ topSpacing: 0 });
-});
-
-
-(function($){
-  $(document).ready(function() {  
-  /* This code is executed after the DOM has been completely loaded */
-    
-    $("#nav li a, a.scrool").click(function(e){
-
-      var full_url = this.href;
-      var parts = full_url.split("#");
-      var trgt = parts[1];
-      if (trgt == null) {
-        return;
-      }
-      var target_offset = $("#"+trgt).offset();
-      var target_top = target_offset.top;
-      
-      $('html,body').animate({scrollTop:target_top -70}, 1000);
-        return false;
-      
-    });
-        
-  });
-})(jQuery);
-
-$(function(){
- var shrinkHeader = 5;
-  $(window).scroll(function() {
-  var scroll = getCurrentScroll();
-    if ( scroll >= shrinkHeader ) {
-       $('#header_wrapper').addClass('shrink');
-    }
-    else {
-      $('#header_wrapper').removeClass('shrink');
-    }
-  });
-function getCurrentScroll() {
-  return window.pageYOffset;
-  }
-});
-
-
-
-function connectSun(){
-
-  $('.connect_sun').height( $('.connect_sun').width() );
-  var container = $('.connect_sun').width() / 2;
-
-  function isEven(n) {
-    return n % 2 == 0;
-  }
-
-  $('.connect_sun--ul').css({ 'width': container, 'height': container, 'margin-top': container, 'margin-left': container });
-
-  $('.connect_sun--ul li').each(function(){
-    var length = $('.connect_sun--ul li').length,
-        thisIndex = $(this).index() + 1,
-        angle = ( ( 360 / length ) * 0.0174532925 ) * thisIndex,
-        R = 0;
-
-    if( isEven(thisIndex) ){
-      R = container * 0.5
-    } else {
-      R = container * 0.7
-    }
-
-    var posY = (R * (Math.sin(angle))),
-        posX = (R * (Math.cos(angle)));
-
-    $(this).css({"top": posY, "left": posX});
-
-  });
-
 }
-connectSun();
-
-$(window).resize(function(){
-  connectSun();
-});
-
-
-
-function isScrolledIntoView(elem){
-  var $elem = $(elem);
-  var $window = $(window);
-
-  var docViewTop = $window.scrollTop();
-  var docViewBottom = docViewTop + $window.height();
-
-  var elemTop = $elem.offset().top;
-  var elemBottom = elemTop + $elem.height();
-
-  return ((elemBottom <= docViewBottom) && (elemTop >= docViewTop));
-}
-function focusAfterText(e){
-  var el = e.get(0);
-  var elemLen = el.value.length;
-
-  el.selectionStart = elemLen;
-  el.selectionEnd = elemLen;
-  el.focus();
-}
-$(window).scroll(function(){
-   if ( isScrolledIntoView( $('.pricing_form--input') ) ){
-      focusAfterText( $('.pricing_form--input') )
-   }
-});
-
-
-
-
-
-function pricingChange(value){
-  $('.pricing_form--year').text( value * 49 );
-  $('.pricing_form--month').text( value * 59 );
-}
-
-function isNumeric(n) {
-  return !isNaN(parseFloat(n)) && isFinite(n);
-}
-
-// price Calculator
-$('body').on('keyup keydown', '.pricing_form--input', function(){
-  var thisVal = parseInt( $(this).val() );
-
-  if( isNumeric( thisVal ) ){
-    pricingChange(thisVal)
-  } else {
-    $(this).val('');
-  }
-});
-
-function plusMinus() {
-  var input = $('.pricing_form--input');
-
-  function goUp(value){
-    if (isNaN(value)) {
-      input.val( 1 );
-    } else {
-      input.val( value+1 );
-      pricingChange( value+1 )
-    }
-    focusAfterText( $('.pricing_form--input') )
-  }
-  function goDn(value){
-    if( value > 1 ){
-      input.val( value-1 );
-      pricingChange( value-1 )
-    } else {
-      input.val( 1 );
-      pricingChange( 1 )
-    }
-    focusAfterText( $('.pricing_form--input') )
-  }
-
-  $('body').on('click', '.pricing_form--up', function(){
-    var thisVal = parseInt( input.val() )
-    goUp( thisVal );
-  });
-  $('body').on('click', '.pricing_form--dn', function(){
-    var thisVal = parseInt( input.val() )
-    goDn( thisVal );
-  });
-
-  $(document).keydown(function(e) {
-    if ( e.keyCode === 38 || e.keyCode === 39 ){
-      goUp( parseInt( input.val() ) );
-    }
-    if ( e.keyCode === 40 || e.keyCode === 37 ){
-      goDn( parseInt( input.val() ) );
-    }
-});
-
-}
-plusMinus();
